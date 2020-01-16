f35d63eb415a53b2aaa6a4806def1933ad24f1f0
Rodrigo Borges <rodrigoborges93@gmail.com>
2020-01-16 18:54:30 +0000

    finished the basic aspects of the web app

--- semanaomnistack10/web/src/components/devs/devs.component.jsx
+++ semanaomnistack10/web/src/components/devs/devs.component.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import api from '../../services/api';
 
-const Devs = ({avatar_url, name, techs, bio, github_username}) => (
+function Devs({avatar_url, name, techs, bio, github_username, _id}){
+
+    const deleteDev = async (id) => {
+        const response = await api.delete('/devs', {_id: id});
+        console.log(response);
+    }
+
+    return (
     <li className='dev-item'>
                 <header>
                     <img src={avatar_url} alt={name} />
@@ -11,6 +19,8 @@ const Devs = ({avatar_url, name, techs, bio, github_username}) => (
                 </header>
                 <p> {bio ? bio : 'There is no Bio for this profile'} </p>
                 <a href={`https://github.com/${github_username}`}> Acessar perfil no Github</a>
+                <button onClick={() => deleteDev(_id)}>Delete</button> 
     </li>
 )
+}
 export default Devs;
