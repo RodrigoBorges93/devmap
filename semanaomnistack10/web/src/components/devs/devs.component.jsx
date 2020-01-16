30e64c9419d7c10b86c52382c87b8d0880ddb6f1
Rodrigo Borges <rodrigoborges93@gmail.com>
2020-01-16 19:12:51 +0000

    adjusted the techs that were appearing all together git push -u origin master clear

--- semanaomnistack10/web/src/components/devs/devs.component.jsx
+++ semanaomnistack10/web/src/components/devs/devs.component.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import api from '../../services/api';
+
 
 function Devs({avatar_url, name, techs, bio, github_username, _id}){
-
-    const deleteDev = async (id) => {
-        const response = await api.delete('/devs', {_id: id});
-        console.log(response);
-    }
 
     return (
     <li className='dev-item'>
@@ -14,12 +9,11 @@ function Devs({avatar_url, name, techs, bio, github_username, _id}){
                     <img src={avatar_url} alt={name} />
                     <div className='user-info'>
                         <strong> {name} </strong>
-                        <span> {techs} </span>
+                        <span> {techs.join(', ')} </span>
                     </div>
                 </header>
                 <p> {bio ? bio : 'There is no Bio for this profile'} </p>
                 <a href={`https://github.com/${github_username}`}> Acessar perfil no Github</a>
-                <button onClick={() => deleteDev(_id)}>Delete</button> 
     </li>
 )
 }
